811c8db9c97e8df7b7eafe72a60081716566ab9f
Roman <jrj80mail@gmail.com>
2023-10-12 22:05:53 +0300

    splitting to componentDidUpdate & fetchImages()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,27 +20,57 @@ export class App extends Component {
     selectedImageUrl: '',
     load: false,
     error: false,
+    loadMore: true,
   };
 
-  async componentDidUpdate(_, prevState) {
+  // async componentDidUpdate(_, prevState) {
+  //   if (
+  //     prevState.query !== this.state.query ||
+  //     prevState.page !== this.state.page
+  //   ) {
+  //     try {
+  //       this.setState({ load: true, error: false });
+  //       const responce = await getImages(this.state.query, this.state.page);
+  //       this.setState({
+  //         image: [...this.state.image, ...responce.data.hits],
+  //         totalImage: responce.data.totalHits,
+  //       });
+  //     } catch {
+  //       this.setState({ error: true });
+  //     } finally {
+  //       this.setState({ load: false });
+  //     }
+  //   }
+  // }
+
+  componentDidUpdate(_, prevState) {
     if (
       prevState.query !== this.state.query ||
       prevState.page !== this.state.page
     ) {
-      try {
-        this.setState({ load: true, error: false });
-        const responce = await getImages(this.state.query, this.state.page);
-        this.setState({
-          image: [...this.state.image, ...responce.data.hits],
-          totalImage: responce.data.totalHits,
-        });
-      } catch {
-        this.setState({ error: true });
-      } finally {
-        this.setState({ load: false });
-      }
+      this.fetchImages();
     }
   }
+
+  fetchImages = async () => {
+    try {
+      this.setState({ load: true, error: false });
+
+      const response = await getImages(this.state.query, this.state.page);
+
+      this.setState(prevState => ({
+        image: [...prevState.image, ...response.data.hits],
+        totalImage: response.data.totalHits,
+        loadMore:
+          this.state.page < Math.ceil(response.data.totalHits / PER_PAGE),
+      }));
+    } catch (error) {
+      // this.setState({ error: true });
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ load: false });
+    }
+  };
 
   getQuery = e => {
     e.preventDefault();
